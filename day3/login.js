f242f8b4ef01f81909a6c8f80792d63410b8ff5f
Mohammed Suhail <111170014+Suhailjms@users.noreply.github.com>
2023-07-13 11:43:18 +0530

    Add files via upload

---
+++ day3/login.js
@@ -0,0 +1,36 @@
+import './login.css';
+import {Link} from 'react-router-dom';
+import React from 'react';
+function Login(){
+
+    return( 
+        <div className="center1">
+        <h1> Login</h1>
+        <form method="post1">
+        <div className="txt_field1">
+        <input type="email" name="e-mail" required></input>
+        <span></span>
+        <label>Your Email</label>
+        </div>
+        <div className="txt_field1">
+        <input type="password" required ></input>
+        <span></span>
+        <label>Password</label>
+        </div>
+        <div className="pass1">Forgot Password?</div>
+        <div className="button">
+
+      <Link to="/home">
+      <input type="submit" value="Login" />
+    </Link>
+
+        </div>
+      <div className="signup_link1">
+      Not a member? <Link to="/signup">SignUp</Link>
+      </div>
+      </form>
+        </div>
+    );
+    }
+ export default Login;
+
